23453157a9221a9ab46ff03e8372a2f71d9ef5fb
William Caetano <williamcaetano14@gmail.com>
2018-07-03 17:23:46 -0300

    Added convert_password to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,14 @@ class User < ApplicationRecord
     validates :name, length: { minimum: 3, maximum: 32 }
     validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
 
+    before_create :convert_password
+    before_update :convert_password
+
+    private
+
+    def convert_password
+        converted_password = Base64.encode64(self.password)
+        self.password = converted_password
+    end
+
 end
