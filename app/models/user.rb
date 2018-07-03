abfa03c2cdc2c58c1a34c5547f42c354216bb054
William Caetano <williamcaetano14@gmail.com>
2018-07-03 17:16:57 -0300

    Added validates to name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
 
     validates :password, length: { minimum: 8, too_long: "Não é permitido menos que #{count} caracteres" }
+    validates :name, length: { minimum: 3, maximum: 32 }
 
 end
