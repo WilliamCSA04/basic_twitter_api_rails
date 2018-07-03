ee5e4db87d283e1418bebacf4ea7d1694a83a7d5
William Caetano <williamcaetano14@gmail.com>
2018-07-03 17:18:28 -0300

    Added validates to email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
 
     validates :password, length: { minimum: 8, too_long: "Não é permitido menos que #{count} caracteres" }
     validates :name, length: { minimum: 3, maximum: 32 }
+    validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
 
 end
