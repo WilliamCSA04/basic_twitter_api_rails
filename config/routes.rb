2abb48546fb3ceb2566556088b61c581c31b6db1
William Caetano <williamcaetano14@gmail.com>
2018-07-04 15:24:42 -0300

    Added users routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # Users routes
+  get    'user/:user_id' => 'users#index'
+  get    'users' => 'users#get'
+  post   'user' => 'users#create'
+  put    'user' => 'users#update'
+  delete 'user' => 'users#delete'
+
 end
