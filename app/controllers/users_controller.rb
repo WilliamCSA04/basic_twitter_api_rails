73c4418fbc319b66c16234cf6d39c8059c826d6d
William Caetano <williamcaetano14@gmail.com>
2018-07-04 14:40:23 -0300

    Added index method to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+
+    def index
+        users = User.find_by_id(params[:user_id])
+        if users.nil?
+            render(json: {error: "Usuario não encontrado"}, status: :unprocessable_entity)
+        else
+            render(json: users.as_json, status: :ok)
+        end    
+    end
+
 end
