fc28bb1e6ac1540b30ad7ff62f8f7b7bb3e24aea
William <williamcaetano14@gmail.com>
2018-07-04 20:05:03 -0300

    Created login to user

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,5 +1,29 @@
 class LoginController < ApplicationController
 
-    
+    before_action :set_user
+
+    def login
+        if @user.login(login_params)
+            render(json: @user.as_json, status: :ok)
+        else
+            render(json: {error: "Usuario não encontrado"}, status: :unprocessable_entity) and return            
+        end
+    end
+
+    private
+
+    #Search for user and return if not find
+    def set_user
+        user = User.find_by_id(params[:user_id])
+        if user.nil?
+            render(json: {error: "Usuario não encontrado"}, status: :unprocessable_entity) and return
+        else
+            @user = user
+        end
+    end
+
+    def login_params
+        params.permit(:password)
+    end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,12 +10,26 @@ class User < ApplicationRecord
         serializable_hash(options)
     end
 
+    def login(password)
+        if is_password_correct?(password)
+            self.update(logged: true)
+        else
+            false
+        end
+    end
+
     private
 
     # Converte senha para encode64
     def convert_password
         converted_password = Base64.encode64(self.password)
         self.password = converted_password
+    end
+
+    def is_password_correct?(password)
+        converted_real_password = Base64.encode64(self.password)
+        converted_parameter_password = Base64.encode64(password)
+        converted_parameter_password == converted_real_password
     end
 
 end
