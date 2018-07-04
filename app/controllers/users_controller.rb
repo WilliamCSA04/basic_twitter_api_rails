89069e53f132224fa258f6355fc57713c8108d47
William Caetano <williamcaetano14@gmail.com>
2018-07-04 14:41:14 -0300

    Changed variable name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,13 @@
 class UsersController < ApplicationController
 
     def index
-        users = User.find_by_id(params[:user_id])
-        if users.nil?
+        user = User.find_by_id(params[:user_id])
+        if user.nil?
             render(json: {error: "Usuario não encontrado"}, status: :unprocessable_entity)
         else
-            render(json: users.as_json, status: :ok)
+            render(json: user.as_json, status: :ok)
         end    
     end
+
 
 end
