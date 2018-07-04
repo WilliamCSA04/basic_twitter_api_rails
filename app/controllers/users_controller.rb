fcb1d0509268f6f3066dfe296566c9e810df55f7
William Caetano <williamcaetano14@gmail.com>
2018-07-04 15:09:47 -0300

    Added set_user method to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,9 @@
 class UsersController < ApplicationController
 
+    before_action :set_user, except: [:get, :create]
+
     def index
-        user = User.find_by_id(params[:user_id])
-        if user.nil?
-            render(json: {error: "Usuario não encontrado"}, status: :unprocessable_entity)
-        else
-            render(json: user.as_json, status: :ok)
-        end    
+        render(json: @user.as_json, status: :ok)       
     end
 
     def get
@@ -17,31 +14,38 @@ class UsersController < ApplicationController
     def create
         user = User.new(user_params)
         if user.save
-            render(json: users.as_json, status: :ok)
+            render(json: user.as_json, status: :ok)
         else
             render(json: {error: "Erro ao salvar usuário"}, status: :ok)            
         end
     end
 
     def update
-        user = User.find(params[:user_id])
-        if user.update(user_params)
-            render(json: users.as_json, status: :ok)            
+        if @user.update(user_params)
+            render(json: @user.as_json, status: :ok)            
         else
             render(json: {error: "Erro ao salvar usuário"}, status: :ok)            
         end
     end
 
     def delete
-        user = User.find(params[:user_id])
-        if user.destroy
-            render(json: users.as_json, status: :ok)                    
+        if @user.destroy
+            render(json: @user.as_json, status: :ok)                    
         else
             render(json: {error: "Erro ao salvar usuário"}, status: :ok)            
         end
     end
 
     private
+
+    def set_user
+        user = User.find_by_id(params[:user_id])
+        if user.nil?
+            render(json: {error: "Usuario não encontrado"}, status: :unprocessable_entity) and return
+        else
+            @user = user
+        end
+    end
 
     def user_params
         params.permit(:email, :name, :password)
